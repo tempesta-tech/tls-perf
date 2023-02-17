3d94ff98eef47210b210b6bf0a74aa20f7c4f3c2
Dmitry Petrov <dp@tempesta-tech.com>
2023-02-17 15:42:10 +0300

    Update copyright on changed files

--- main.cc
+++ main.cc
@@ -1,7 +1,7 @@
 /**
  *		TLS handshakes benchmarking tool.
  *
- * Copyright (C) 2020-2021 Tempesta Technologies, INC.
+ * Copyright (C) 2020-2023 Tempesta Technologies, INC.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by
